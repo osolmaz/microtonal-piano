805dee2d7b8d7a41cf5fbd26c730e6d49908be0c
osolmaz <onursolmaz@gmail.com>
2022-10-18 15:18:16 +0200

    Updates

---
+++ precache-manifest.31749b1fec1cc837e10afe8e8d8dcec2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b561ab7e6b951d236831cfe97e7c854d",
+    "url": "/microtonal-piano/index.html"
+  },
+  {
+    "revision": "eb9c57aa0283fbf7e432",
+    "url": "/microtonal-piano/static/css/main.b6a2175e.chunk.css"
+  },
+  {
+    "revision": "fc7440dca79ea19caa2a",
+    "url": "/microtonal-piano/static/js/2.ac0d7597.chunk.js"
+  },
+  {
+    "revision": "f6d876448b8e86539477abef92b805c7",
+    "url": "/microtonal-piano/static/js/2.ac0d7597.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eb9c57aa0283fbf7e432",
+    "url": "/microtonal-piano/static/js/main.ca091a48.chunk.js"
+  },
+  {
+    "revision": "f69a894e1934b3cbc243",
+    "url": "/microtonal-piano/static/js/runtime-main.0751daad.js"
+  }
+]);
